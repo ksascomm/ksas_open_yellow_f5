aef7a774660a95a7dbc6ed740d154296f3fdf109
ksascomm <ksasweb@jhu.edu>
2015-07-13 16:02:19 -0400

    added outbound links & viewport sizing GA tracking

--- parts-analytics.php
+++ parts-analytics.php
@@ -13,3 +13,71 @@
   ga('globalKSAS.send', 'pageview');
 
 </script>
+
+<script>
+function _gaLt(event){
+    var el = event.srcElement || event.target;
+
+    /* Loop up the DOM tree through parent elements if clicked element is not a link (eg: an image inside a link) */
+    while(el && (typeof el.tagName == 'undefined' || el.tagName.toLowerCase() != 'a' || !el.href)){
+        el = el.parentNode;
+    }
+
+    if(el && el.href){
+        /* link */
+        var link = el.href;
+        if(link.indexOf(location.host) == -1 && !link.match(/^javascript\:/i)){ /* external link */
+            /* HitCallback function to either open link in either same or new window */
+            var hitBack = function(link, target){
+                target ? window.open(link, target) : window.location.href = link;
+            };
+            /* Is target set and not _(self|parent|top)? */
+            var target = (el.target && !el.target.match(/^_(self|parent|top)$/i)) ? el.target : false;
+            /* send event with callback */
+            ga(
+                "send", "event", "Outgoing Links", link,
+                document.location.pathname + document.location.search,
+                {"hitCallback": hitBack(link, target)}
+            );
+
+            /* Prevent standard click */
+            event.preventDefault ? event.preventDefault() : event.returnValue = !1;
+        }
+
+    }
+}
+
+/* Attach the event to all clicks in the document after page has loaded */
+var w = window;
+w.addEventListener ? w.addEventListener("load",function(){document.body.addEventListener("click",_gaLt,!1)},!1)
+ : w.attachEvent && w.attachEvent("onload",function(){document.body.attachEvent("onclick",_gaLt)});
+</script>
+
+<script type="text/javascript">
+function viewport() {
+  var myWidth = 0, myHeight = 0;
+  if( typeof( window.innerWidth ) == 'number' ) {
+  //Non-IE
+  myWidth = window.innerWidth;
+  myHeight = window.innerHeight;
+  } else if( document.documentElement &&
+ ( document.documentElement.clientWidth
+ || document.documentElement.clientHeight ) ) {
+  //IE 6+ in 'standards compliant mode'
+  myWidth = document.documentElement.clientWidth;
+  myHeight = document.documentElement.clientHeight;
+  } else if( document.body &&
+ ( document.body.clientWidth
+ || document.body.clientHeight ) ) {
+  //IE 4 compatible
+  myWidth = document.body.clientWidth;
+  myHeight = document.body.clientHeight;
+  }
+  ga('send',
+   'event',
+   'Viewport',
+   'Size',
+   myWidth+'x'+myHeight,
+   {'nonInteraction': 1});
+}
+</script>
