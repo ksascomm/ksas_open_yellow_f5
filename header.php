75d75735065fbe7198842ddd385813112e0f9dfd
ksascomm <ksasweb@jhu.edu>
2015-07-31 09:55:34 -0400

    mobile/meanmenu breakpoint edits

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+  <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
@@ -70,7 +70,7 @@
 
 		<div id="desktop-nav">
 			<div class="row hide-for-print">
-				<div id="search-bar" class="small-12 large-5 large-offset-7 columns">
+				<div id="search-bar" class="small-12 medium-5 medium-offset-7 columns">
 					<div class="row">
 						<div class="small-6 columns">
 						<?php $theme_option = flagship_sub_get_global_options(); 
@@ -97,8 +97,12 @@
 			</div>
 			<div class="row">
 				<div class="small-12 columns" id="logo_nav">
-					<li class="logo"><a href="http://krieger.jhu.edu" title="Krieger School of Arts & Sciences">Krieger School of Arts & Sciences</a></li>
-			<h1><a class="white" href="<?php echo site_url(); ?>"><span class="small"><?php echo get_bloginfo ( 'description' ); ?></span><?php echo get_bloginfo( 'title' ); ?></a></h1>
+					<div class="medium-3 columns">
+						<li class="logo"><a href="<?php echo network_home_url(); ?>" title="Krieger School of Arts & Sciences"><img src="<?php echo get_template_directory_uri() ?>/assets/images/ksas-logo.png" alt="jhu logo"></a></li>
+					</div>
+					<div class="medium-9 columns">
+						<h1><a class="white" href="<?php echo site_url(); ?>"><span class="small"><?php echo get_bloginfo ( 'description' ); ?></span><?php echo get_bloginfo( 'title' ); ?></a></h1>
+					</div>
 				</div>
 			</div>
 			<div class="row hide-for-print">
