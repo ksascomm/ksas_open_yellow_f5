f82df40e55ab12c2a33de7515a6548613ecb2c45
ksascomm <ksasweb@jhu.edu>
2016-04-05 14:13:52 -0400

    faculty books edits

--- parts-faculty-books.php
+++ parts-faculty-books.php
@@ -21,17 +21,21 @@
     
     
 	if ( $single_books_query->have_posts() ) : while ($single_books_query->have_posts()) : $single_books_query->the_post(); ?>
-		<div class="small-12 columns book-entry">
-			<a href="<?php the_permalink(); ?>">
-	    		<?php if ( has_post_thumbnail()) {  the_post_thumbnail('directory', array('class'	=> "floatleft"));  } ?>	
-				<h5><?php the_title();?></h5>
-				<h6><?php if ( get_post_meta($post->ID, 'ecpt_pub_date', true) ) : echo get_post_meta($post->ID, 'ecpt_pub_date', true);  endif; ?><?php if ( get_post_meta($post->ID, 'ecpt_publisher', true) ) :?>, <?php echo get_post_meta($post->ID, 'ecpt_publisher', true);  endif; ?></h6>
-				<p><strong>Role:&nbsp;<span style="text-transform:capitalize;"><?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); ?></span>
-				<?php if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on') { $faculty_post_id2 = get_post_meta($post->ID, 'ecpt_pub_author2', true); ?><br>
+		<article class="small-12 columns book-entry">
+	    		<?php if ( has_post_thumbnail()) {  the_post_thumbnail('directory', array('class'	=> "floatleft"));  } ?>
+	    		<ul class="no-bullet">
+	    			<li><strong><a href="<?php the_permalink(); ?>"><?php the_title();?></a></strong></li>
+					<li><?php if ( get_post_meta($post->ID, 'ecpt_pub_date', true) ) : echo get_post_meta($post->ID, 'ecpt_pub_date', true);  endif; ?><?php if ( get_post_meta($post->ID, 'ecpt_publisher', true) ) :?>, <?php echo get_post_meta($post->ID, 'ecpt_publisher', true);  endif; ?></li>
+					<li><strong>Role:</strong> <span style="text-transform:capitalize;"><?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); ?></span></li>
+					<li><?php if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on') { $faculty_post_id2 = get_post_meta($post->ID, 'ecpt_pub_author2', true); ?><br>
 				   <?php echo get_the_title($faculty_post_id2); ?>,&nbsp;<?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); ?>
-				<?php } ?>
-				</strong></p>
-			</a>
-		</div>
+				<?php } ?></li>
+					<li><?php if ( get_post_meta($post->ID, 'ecpt_pub_link', true) ) :?> 
+							<a href="http://<?php echo get_post_meta($post->ID, 'ecpt_pub_link', true); ?>">
+								Purchase Online <span class="fa fa-external-link-square"></span>
+							</a>
+						<?php endif; ?></li>
+	    		</ul>
+		</article>
 	<?php endwhile; endif;  wp_reset_query(); ?>
 </div>
